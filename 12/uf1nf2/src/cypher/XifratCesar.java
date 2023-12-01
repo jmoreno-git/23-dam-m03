9802bc7f18e0be963134f2bd1a7c338aeaec922d
jose <jmoreno@proven.cat>
2023-12-01 14:00:54 +0100

    xifrat cesar

--- 12/uf1nf2/src/cypher/XifratCesar.java
+++ 12/uf1nf2/src/cypher/XifratCesar.java
@@ -21,7 +21,7 @@ public class XifratCesar {
         for (int i = 0; i < missatge.length(); i++) {
             char c1 = missatge.charAt(i);
             int index1 = lletres.indexOf(c1);
-            int index2 = index1 + desp;
+            int index2 = (index1 + desp)%lletres.length();
             char c2 = lletres.charAt(index2);
             result += c2;
         }
@@ -37,7 +37,18 @@ public class XifratCesar {
      */
     public static String desxifrarCesar(String missatge, int desp) {
         String result = "";
-        //TODO
+        final String lletres = "abcdefghijklmnopqrstuvwxyz1234567890 ,;:";
+        missatge = missatge.toLowerCase();
+        for (int i = 0; i < missatge.length(); i++) {
+            char c1 = missatge.charAt(i);
+            int index1 = lletres.indexOf(c1);
+            int index2 = (index1 - desp)%lletres.length();
+            if (index2 < 0) {
+                index2 += lletres.length();
+            }
+            char c2 = lletres.charAt(index2);
+            result += c2;
+        }
         return result;
     }
 }
